08c2597f9ff03087f4fb2592c569197060539d18
Jesse-D-Miller <jessedonaldmiller@gmail.com>
2025-05-30 18:09:42 -0700

    view exists now for new category, clicking add errors out, working on a fix

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,7 +1,22 @@
 class Admin::CategoriesController < ApplicationController
+http_basic_authenticate_with name: ENV["ADMIN_USERNAME"], password: ENV["ADMIN_PASSWORD"]
 
   def index
     @categories = Category.all
+  end
+
+  def new
+    @categories = Category.new
+  end
+
+  def create
+    @categories = Category.new(category_params)
+
+    if @categories.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
   end
 
 end
